e67f73684217cef57ef385182af24aeff6fb8812
epifanio <epiesasha@me.com>
2016-05-31 16:53:52 -0400

    removed packages from conda std repo

--- install_scripts/conda.sh
+++ install_scripts/conda.sh
@@ -6,21 +6,10 @@ conda config --set ssl_verify false
 
 conda install -y -n $MY_CONDA_PY3ENV \
                                'pandas' \
-                               'oceans' \
-                               'nco' \
-                               'gsl' \
-                               'ctd' \
                                'vispy' \
-                               'seawater' \
-                               'pyepsg' \
-                               'folium' \
-                               'geopandas' \
                                'mpld3' \
-                               'mplleaflet' \
-                               'oct2py' \
                                'proj4' \
                                'bokeh' \
                                'gdal' \
                                'pyproj' \
-                               'qrcode' \
                                && conda clean -yt
