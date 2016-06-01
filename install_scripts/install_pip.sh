25385447ca4aa49e9a3ec6acb338fa2b1e25a494
epifanio <epiesasha@gmail.com>
2016-06-01 01:26:35 -0400

    add pip dependencies

---
+++ install_scripts/install_pip.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+/home/main/anaconda2/envs/python3/bin/pip install -U git+https://github.com/kynan/nbstripout
+/home/main/anaconda2/envs/python3/bin/pip install -U  git+https://github.com/cleder/pygeoif
+/home/main/anaconda2/envs/python3/bin/pip install -U pytz
+/home/main/anaconda2/envs/python3/bin/pip install -U  git+https://github.com/cleder/czml
+/home/main/anaconda2/envs/python3/bin/pip install -U  git+https://github.com/sinhrks/cesiumpy
+/home/main/anaconda2/envs/python3/bin/pip install -U  git+https://github.com/mapbox/rasterio
