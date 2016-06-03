604210a8067c490df6f89a9b76f9f5fac59fb5f4
epifanio <epiesasha@me.com>
2016-06-02 20:33:39 -0400

    debug build

--- install_scripts/getdata.sh
+++ install_scripts/getdata.sh
@@ -10,35 +10,6 @@ mkdir -p $DATA_FOLDER
 mkdir -p $GRASS_DATA_FOLDER
 mkdir -p $NE2_DATA_FOLDER
 
-FILES="shape rast_geotiff kml"
-
-for FILE in $FILES ; do
-   wget -N --progress=dot:mega "$BASE_URL/nc_$FILE.tar.gz"
-done
-
-wget -N http://www.grassbook.org/grasslocations/nc_epsg_codes.html
-wget -N http://grass.osgeo.org/sampledata/north_carolina/README.html
-
-mv nc_epsg_codes.html $DATA_FOLDER/
-mv README.html $DATA_FOLDER/
-
-#and install them ...
-#cd "$GRASS_DATA_FOLDER"
-
-for FILE in $FILES ; do
-    if [[ "$FILE" != rast_geotiff ]]
-        then
-            tar xzf "nc_$FILE.tar.gz"
-            chgrp users "$FILE"
-            chmod g+w "$FILE"
-			mv "nc$FILE/" $GRASS_DATA_FOLDER/
-    fi
-    if [[ "$FILE" == rast_geotiff ]]
-        then
-		    tar xzf "nc_$FILE.tar.gz"
-	        mv "ncrast" $GRASS_DATA_FOLDER/
-    fi
-done
 
 
 wget "http://grass.osgeo.org/sampledata/north_carolina/nc_basic_spm_grass7.tar.gz"
@@ -108,22 +79,3 @@ done
 mv * /home/main/notebooks/data/netcdf/
 cd ..
 rm -rf netcdf
-
-# Cape Cod SRTM and LANDSAT  (this part is disble because of disc space issue)
-
-mkdir -p $DATA_FOLDER/landsat/
-DATA_URL="http://download.osgeo.org/livedvd/data/ossim/"
-BASENAME="p011r031_7t19990918_z19_nn"
-for BAND in 10 20 30 ; do
-    # LANDSAT
-    wget --progress=dot:mega "$DATA_URL/ossim_data/${BASENAME}$BAND.tif" \
-         --output-document="$DATA_FOLDER/landsat/${BASENAME}$BAND.tif"
-done
-wget --progress=dot:mega "$DATA_URL/ossim_data/p011r031_7t19990918_z19_nn80.tif" \
-         --output-document="$DATA_FOLDER/landsat/p011r031_7t19990918_z19_nn80.tif"
-
-wget --progress=dot:mega "$DATA_URL/ossim_data/SRTM_fB03_p011r031.tif" \
-         --output-document="$DATA_FOLDER/landsat/SRTM_fB03_p011r031.tif"
-
-wget  --progress=dot:mega "http://download.osgeo.org/livedvd/data/ossim/ossim_data/ossim-dem-color-table-template.kwl" \
-         --output-document="$DATA_FOLDER/landsat/ossim-dem-color-table-template.kwl"
